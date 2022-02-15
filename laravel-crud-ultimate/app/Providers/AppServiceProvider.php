2597a450817fa6bd27a3422b0492bc3e2a91c0ac
bryansbr <bryan.biojo@correounivalle.edu.co>
2022-02-15 13:42:10 -0500

    Line added for fix possibles migration errors.

--- laravel-crud-ultimate/app/Providers/AppServiceProvider.php
+++ laravel-crud-ultimate/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; // Add for run
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191); // Add for run
     }
 }
